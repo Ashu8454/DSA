c94fb7cdeca42804994002da467553fcc0e9325d
Ashu Rai <arai07684@gmail.com>
2026-07-31 20:01:09 +0530

    Time: 9 ms (94.22%) | Memory: 48.1 MB (52.60%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+        for (int i = 0; i < word.length(); i++) {
+            freq[word.charAt(i) - 'a']++;
+        }
+        
+        Arrays.sort(freq);
+        
+        int pushes = 0;
+        int count = 0;
+        
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] == 0) {
+                break;
+            }
+            
+            int position = (count / 8) + 1;
+            pushes += freq[i] * position;
+            count++;
+        }
+        
+        return pushes;
+    }
+}
